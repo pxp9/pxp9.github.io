01cac01cbb451befb27e295e7a188cf9866514fe
pxp9 <48651252+pxp9@users.noreply.github.com>
2022-08-05 17:48:05 +0200

    Hugo blog

--- shell.nix
+++ shell.nix
@@ -2,14 +2,6 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "env";
   buildInputs = [
-    ruby
-    rubyPackages.jekyll-feed
-    jekyll
-    libpcap
-    libxml2
-    libxslt
-    pkg-config
-    bundix
-    gnumake
-  ];
+   hugo
+   ];
 }

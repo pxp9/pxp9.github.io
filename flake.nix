462b8f2e55f0757d3ef70f4493d270922929687c
pxp9 <pepe.marquezromero@protonmail.com>
2025-11-28 14:41:19 +0200

    make bigger the text in the diagram and fix grammar mistakes

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A Nix-flake-based Elixir development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            overlays = [];
+            packages =
+              with pkgs;
+              [
+                zola
+                claude-code
+              ];
+          };
+        }
+      );
+    };
+}
+
